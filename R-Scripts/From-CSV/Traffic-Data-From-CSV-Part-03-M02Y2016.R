83c6acc67268102ba6d3600b982ddbc5c5c05932
David Mills <david@davidmills.io>
2020-09-23 23:41:57 -0500

    Same script, for different months.
    Could be more efficiently written as a single script with a loop in the future.

---
+++ R-Scripts/From-CSV/Traffic-Data-From-CSV-Part-03-M02Y2016.R
@@ -0,0 +1,75 @@
+install.packages("reshape")
+
+library("reshape")
+
+# Read Travel Sensors CSV into R.
+TravelSensors <- read.csv(file="Travel_Sensors.csv", header=TRUE, sep=",")
+
+#Transposes/Reshapes the data using melt and cast.
+M02_24H_HL <- melt(M02_24H_HL,id.vars=c("Reader.Identifier","HourCorrected"))
+M02_24H_HL <- as.data.frame(cast(M02_24H_HL,Reader.Identifier~HourCorrected~variable))
+
+# Creates a new dataframe for "Normalized" data.
+M02_24H_HL_normalized <- M02_24H_HL
+
+# Gets the Max Value for Each Row for the new Normalized Dataframe.
+M02_24H_HL_normalized$max <- apply(M02_24H_HL_normalized,1,max, na.rm=T)
+
+# Sets i and x values to run through 24 iterations.
+i = 1
+x = 24
+
+while(i <= x)
+{
+  # Divides first column
+  M02_24H_HL_normalized[,i] <- as.numeric(paste(M02_24H_HL_normalized[,i])) / as.numeric(as.character(M02_24H_HL_normalized$max))
+  # Add 1 to i
+  i = i + 1
+}
+
+# Removes the max column
+M02_24H_HL_normalized$max <- NULL
+
+# Creates an empty column to store the array.
+M02_24H_HL_normalized$twentyfourhours.n <- NA
+# Adds all of the values into column twentyfourhours with "," as the separator.
+M02_24H_HL_normalized$twentyfourhours.n <- apply(M02_24H_HL_normalized[, 1:24], 1, paste, collapse=",")
+# Remove all spaces.
+M02_24H_HL_normalized$twentyfourhours.n <- gsub(" ", "", M02_24H_HL_normalized$twentyfourhours)
+# Delete first 24 columns.
+M02_24H_HL_normalized <- M02_24H_HL_normalized[ -c(1:24) ]
+# Put brackets around values in column "twentyfourhours".
+M02_24H_HL_normalized$twentyfourhours.n <- paste0("[",M02_24H_HL_normalized$twentyfourhours, "]")
+
+# Creates an empty column to store the array.
+M02_24H_HL$twentyfourhours <- NA
+# Adds all of the values into column twentyfourhours with "," as the separator.
+M02_24H_HL$twentyfourhours <- apply(M02_24H_HL[, 1:24], 1, paste, collapse=",")
+# Remove all spaces.
+M02_24H_HL$twentyfourhours <- gsub(" ", "", M02_24H_HL$twentyfourhours)
+# Delete first 24 columns.
+M02_24H_HL <- M02_24H_HL[ -c(1:24) ]
+# Put brackets around values in column "twentyfourhours".
+M02_24H_HL$twentyfourhours <- paste0("[",M02_24H_HL$twentyfourhours, "]")
+
+# Merges the normalized dataframe with the un-normalized dataframe.
+M02_24H_HL <- merge(M02_24H_HL, M02_24H_HL_normalized, by.x = "row.names", by.y = "row.names", all.x = T)
+# Merges the travel sensors dataframe which contains the latitude and longitude.
+M02_24H_HL <- merge(M02_24H_HL, TravelSensors, by.x = "Row.names", by.y = "READER_ID", all.x = T)
+
+# Rename the first column from "Row.names"
+colnames(M02_24H_HL)[1] <- "Reader_ID"
+
+# Keep only the desired columns.
+M02_24H_HL <- M02_24H_HL[c(1,2,3,31,32)]
+
+# Adds Longitude and Latitude values that were missed due to naming discrepancies.
+M02_24H_HL$LOCATION_LATITUDE[M02_24H_HL$Reader_ID=="benWhite_riverside"] <- TravelSensors$LOCATION_LATITUDE[TravelSensors$READER_ID=="benwhite_riverside"]
+M02_24H_HL$LOCATION_LONGITUDE[M02_24H_HL$Reader_ID=="benWhite_riverside"] <- TravelSensors$LOCATION_LONGITUDE[TravelSensors$READER_ID=="benwhite_riverside"]
+M02_24H_HL$LOCATION_LATITUDE[M02_24H_HL$Reader_ID=="ih_35_riverside"] <- TravelSensors$LOCATION_LATITUDE[TravelSensors$READER_ID=="riverside_ih35"]
+M02_24H_HL$LOCATION_LONGITUDE[M02_24H_HL$Reader_ID=="ih_35_riverside"] <- TravelSensors$LOCATION_LONGITUDE[TravelSensors$READER_ID=="riverside_ih35"]
+M02_24H_HL$LOCATION_LATITUDE[M02_24H_HL$Reader_ID=="cesar_chavez_ih_35"] <- TravelSensors$LOCATION_LATITUDE[TravelSensors$READER_ID=="cesar_chavez_ih35"]
+M02_24H_HL$LOCATION_LONGITUDE[M02_24H_HL$Reader_ID=="cesar_chavez_ih_35"] <- TravelSensors$LOCATION_LONGITUDE[TravelSensors$READER_ID=="cesar_chavez_ih35"]
+
+# Writes the file to a CSV.
+write.csv(M02_24H_HL, "M02_24H_HL.csv")
---
+++ R-Scripts/From-CSV/Traffic-Data-From-CSV-Part-03-M03Y2016.R
@@ -0,0 +1,79 @@
+install.packages("reshape")
+
+library("reshape")
+
+# Read Travel Sensors CSV into R.
+TravelSensors <- read.csv(file="Travel_Sensors.csv", header=TRUE, sep=",")
+
+#Transposes/Reshapes the data using melt and cast.
+M04_24H_HL <- melt(M04_24H_HL,id.vars=c("Reader.Identifier","HourCorrected"))
+M04_24H_HL <- as.data.frame(cast(M04_24H_HL,Reader.Identifier~HourCorrected~variable))
+
+# Creates a new dataframe for "Normalized" data.
+M04_24H_HL_normalized <- M04_24H_HL
+
+# Gets the Max Value for Each Row for the new Normalized Dataframe.
+M04_24H_HL_normalized$max <- apply(M04_24H_HL_normalized,1,max, na.rm=T)
+
+# Sets i and x values to run through 24 iterations.
+i = 1
+x = 24
+
+while(i <= x)
+{
+  # Divides first column
+  M04_24H_HL_normalized[,i] <- as.numeric(paste(M04_24H_HL_normalized[,i])) / as.numeric(as.character(M04_24H_HL_normalized$max))
+  # Add 1 to i
+  i = i + 1
+}
+
+# Removes the max column
+M04_24H_HL_normalized$max <- NULL
+
+# Creates an empty column to store the array.
+M04_24H_HL_normalized$twentyfourhours.n <- NA
+# Adds all of the values into column twentyfourhours with "," as the separator.
+M04_24H_HL_normalized$twentyfourhours.n <- apply(M04_24H_HL_normalized[, 1:24], 1, paste, collapse=",")
+# Remove all spaces.
+M04_24H_HL_normalized$twentyfourhours.n <- gsub(" ", "", M04_24H_HL_normalized$twentyfourhours)
+# Delete first 24 columns.
+M04_24H_HL_normalized <- M04_24H_HL_normalized[ -c(1:24) ]
+# Put brackets around values in column "twentyfourhours".
+M04_24H_HL_normalized$twentyfourhours.n <- paste0("[",M04_24H_HL_normalized$twentyfourhours, "]")
+
+# Creates an empty column to store the array.
+M04_24H_HL$twentyfourhours <- NA
+# Adds all of the values into column twentyfourhours with "," as the separator.
+M04_24H_HL$twentyfourhours <- apply(M04_24H_HL[, 1:24], 1, paste, collapse=",")
+# Remove all spaces.
+M04_24H_HL$twentyfourhours <- gsub(" ", "", M04_24H_HL$twentyfourhours)
+# Delete first 24 columns.
+M04_24H_HL <- M04_24H_HL[ -c(1:24) ]
+# Put brackets around values in column "twentyfourhours".
+M04_24H_HL$twentyfourhours <- paste0("[",M04_24H_HL$twentyfourhours, "]")
+
+# Merges the normalized dataframe with the un-normalized dataframe.
+M04_24H_HL <- merge(M04_24H_HL, M04_24H_HL_normalized, by.x = "row.names", by.y = "row.names", all.x = T)
+# Merges the travel sensors dataframe which contains the latitude and longitude.
+M04_24H_HL <- merge(M04_24H_HL, TravelSensors, by.x = "Row.names", by.y = "READER_ID", all.x = T)
+
+# Rename the first column from "Row.names"
+colnames(M04_24H_HL)[1] <- "Reader_ID"
+
+# Keep only the desired columns.
+M04_24H_HL <- M04_24H_HL[c(1,2,3,31,32)]
+
+# Adds Longitude and Latitude values that were missed due to naming discrepancies.
+M04_24H_HL$LOCATION_LATITUDE[M04_24H_HL$Reader_ID=="benWhite_riverside"] <- TravelSensors$LOCATION_LATITUDE[TravelSensors$READER_ID=="benwhite_riverside"]
+M04_24H_HL$LOCATION_LONGITUDE[M04_24H_HL$Reader_ID=="benWhite_riverside"] <- TravelSensors$LOCATION_LONGITUDE[TravelSensors$READER_ID=="benwhite_riverside"]
+M04_24H_HL$LOCATION_LATITUDE[M04_24H_HL$Reader_ID=="ih_35_riverside"] <- TravelSensors$LOCATION_LATITUDE[TravelSensors$READER_ID=="riverside_ih35"]
+M04_24H_HL$LOCATION_LONGITUDE[M04_24H_HL$Reader_ID=="ih_35_riverside"] <- TravelSensors$LOCATION_LONGITUDE[TravelSensors$READER_ID=="riverside_ih35"]
+M04_24H_HL$LOCATION_LATITUDE[M04_24H_HL$Reader_ID=="cesar_chavez_ih_35"] <- TravelSensors$LOCATION_LATITUDE[TravelSensors$READER_ID=="cesar_chavez_ih35"]
+M04_24H_HL$LOCATION_LONGITUDE[M04_24H_HL$Reader_ID=="cesar_chavez_ih_35"] <- TravelSensors$LOCATION_LONGITUDE[TravelSensors$READER_ID=="cesar_chavez_ih35"]
+M04_24H_HL$LOCATION_LATITUDE[M04_24H_HL$Reader_ID=="fm_2222_loop360"] <- TravelSensors$LOCATION_LATITUDE[TravelSensors$READER_ID=="fm2222_loop360"]
+M04_24H_HL$LOCATION_LONGITUDE[M04_24H_HL$Reader_ID=="fm_2222_loop360"] <- TravelSensors$LOCATION_LONGITUDE[TravelSensors$READER_ID=="fm2222_loop360"]
+M04_24H_HL$LOCATION_LATITUDE[M04_24H_HL$Reader_ID=="lamar_brodie_oak"] <- TravelSensors$LOCATION_LATITUDE[TravelSensors$READER_ID=="Lamar_BrodieOaks"]
+M04_24H_HL$LOCATION_LONGITUDE[M04_24H_HL$Reader_ID=="lamar_brodie_oak"] <- TravelSensors$LOCATION_LONGITUDE[TravelSensors$READER_ID=="Lamar_BrodieOaks"]
+
+# Writes the file to a CSV.
+write.csv(M04_24H_HL, "M04_24H_HL.csv")
